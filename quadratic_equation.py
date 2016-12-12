2da4dde43d34f46a9f566ec7644c6335aeed48b5
Vadim <megasvdonsk@gmail.com>
2016-12-12 17:58:13 +0000

    Добавил условие для проверки значения дискриминанта

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,11 +1,18 @@
 from math import sqrt
 
+#не хватает проверки условия. Дискриминант может быть больше/меньше нуля либо равно нулю
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
-    if discriminant == 0:
-        return root1, None
+#    root1 = (-b - sqrt(discriminant)) / (2 * a)
+#    root2 = (-b + sqrt(discriminant)) / (2 * a)
+    if discriminant < 0:
+        return None, None 
+#Через elif не получилось, ругалось на NoneType в 3 местах.
     else:
-        return root1, root2
+      root1 = (-b - sqrt(discriminant)) / (2 * a)
+      root2 = (-b + sqrt(discriminant)) / (2 * a)
+    if discriminant == 0:
+       return root1, None
+    else:
+       return root1, root2
